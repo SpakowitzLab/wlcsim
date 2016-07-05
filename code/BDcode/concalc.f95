2565d3b91293034595a0e2254ecf4b168932ced1
LuisNievesRosado <luisnievesrosado@gmail.com>
2016-07-05 14:50:47 -0700

    Comment out pseudo potential

--- code/BDcode/concalc.f95
+++ code/BDcode/concalc.f95
@@ -21,6 +21,7 @@
       INTEGER I,J,IB						! Index holder
       INTEGER RK					! RK integer
       INTEGER BROWN             ! Logic for BD forces
+	  DOUBLE PRECISION TIME     ! Time of BD simulation
       
 ! Variables for use in the constraint calculation
 
@@ -47,11 +48,12 @@
             U(I,1)=R(I+IB+1,1)-R(I+IB,1)
             U(I,2)=R(I+IB+1,2)-R(I+IB,2)
             U(I,3)=R(I+IB+1,3)-R(I+IB,3)
-            B(I)=sqrt(U(I,1)**2.+U(I,2)**2.+U(I,3)**2.)
+            B(I)=SQRT(U(I,1)**2.+U(I,2)**2.+U(I,3)**2.)
             U(I,1)=U(I,1)/B(I)
             U(I,2)=U(I,2)/B(I)
             U(I,3)=U(I,3)/B(I)
  10      CONTINUE
+
 
 ! Setup the A component
 
@@ -74,83 +76,81 @@
 		
 ! Calculate psuedo-potential (if Brownian forces on)
 
-         if (BROWN.EQ.1) then
-            DETLT(1)=1.0
-            DETLT(2)=APS(1)
-            DETGT(N-1)=1.0
-            DETGT(N-2)=APS(N-1)
-            DO 20 I=3,(N-1)
-               DETLT(I)=APS(I-1)*DETLT(I-1)- &
-                    BPS(I-2)*BPS(I-2)*DETLT(I-2)
-               DETGT(N-I)=APS(N-I+1)*DETGT(N-I+1)- &
-                    BPS(N-I+1)*BPS(N-I+1)*DETGT(N-I+2)
- 20         CONTINUE
-            DETER=APS(N-1)*DETLT(N-1)-BPS(N-2)*BPS(N-2)*DETLT(N-2)
-            FPS(1,1)=DETGT(2)*BPS(1)*B(2)*U(2,1)/DETER
-            FPS(1,2)=DETGT(2)*BPS(1)*B(2)*U(2,2)/DETER
-            FPS(1,3)=DETGT(2)*BPS(1)*B(2)*U(2,3)/DETER
-            FPS(2,1)=DETGT(2)*BPS(1)*(B(1)*U(1,1)- &
-                 B(2)*U(2,1))/DETER+ &
-                 DETGT(3)*APS(1)*BPS(2)*B(3)*U(3,1)/DETER
-            FPS(2,2)=DETGT(2)*BPS(1)*(B(1)*U(1,2)- &
-                 B(2)*U(2,2))/DETER+ &
-                 DETGT(3)*APS(1)*BPS(2)*B(3)*U(3,2)/DETER
-            FPS(2,3)=DETGT(2)*BPS(1)*(B(1)*U(1,3)- &
-                 B(2)*U(2,3))/DETER+ &
-                 DETGT(3)*APS(1)*BPS(2)*B(3)*U(3,3)/DETER
-            DO 30 I=3,(N-2)
-               FPS(I,1)=(DETLT(I-1)*DETGT(I)*BPS(I-1)*(-B(I)*U(I,1)+B(I-1)*U(I-1,1))+ &
-                    DETLT(I-1)*DETGT(I+1)*APS(I-1)*BPS(I)*B(I+1)*U(I+1,1)- &
-                    DETLT(I-2)*DETGT(I)*APS(I)*BPS(I-2)*B(I-2)*U(I-2,1)- &
-                    DETLT(I-2)*DETGT(I+1)*( &
-                    (BPS(I-2)**2.)*BPS(I)*B(I+1)*U(I+1,1)- &
-                    (BPS(I)**2.)*BPS(I-2)*B(I-2)*U(I-2,1)))/DETER
-               FPS(I,2)=(DETLT(I-1)*DETGT(I)*BPS(I-1)*(-B(I)*U(I,2)+B(I-1)*U(I-1,2))+ &
-                    DETLT(I-1)*DETGT(I+1)*APS(I-1)*BPS(I)*B(I+1)*U(I+1,2)- &
-                    DETLT(I-2)*DETGT(I)*APS(I)*BPS(I-2)*B(I-2)*U(I-2,2)- &
-                    DETLT(I-2)*DETGT(I+1)*( &
-                    (BPS(I-2)**2.)*BPS(I)*B(I+1)*U(I+1,2)- &
-                    (BPS(I)**2.)*BPS(I-2)*B(I-2)*U(I-2,2)))/DETER
-               FPS(I,3)=(DETLT(I-1)*DETGT(I)*BPS(I-1)*(-B(I)*U(I,3)+B(I-1)*U(I-1,3))+ &
-                    DETLT(I-1)*DETGT(I+1)*APS(I-1)*BPS(I)*B(I+1)*U(I+1,3)- &
-                    DETLT(I-2)*DETGT(I)*APS(I)*BPS(I-2)*B(I-2)*U(I-2,3)- &
-                    DETLT(I-2)*DETGT(I+1)*( &
-                    (BPS(I-2)**2.)*BPS(I)*B(I+1)*U(I+1,3)- &
-                    (BPS(I)**2.)*BPS(I-2)*B(I-2)*U(I-2,3)))/DETER
- 30         CONTINUE
-            FPS(N-1,1)=DETLT(N-2)*BPS(N-2)*(-B(N-1)*U(N-1,1)+ &
-                 B(N-2)*U(N-2,1))/DETER- &
-                 DETLT(N-3)*APS(N-1)*BPS(N-3)*B(N-3)*U(N-3,1)/DETER
-            FPS(N-1,2)=DETLT(N-2)*BPS(N-2)*(-B(N-1)*U(N-1,2)+ &
-                 B(N-2)*U(N-2,2))/DETER- &
-                 DETLT(N-3)*APS(N-1)*BPS(N-3)*B(N-3)*U(N-3,2)/DETER
-            FPS(N-1,3)=DETLT(N-2)*BPS(N-2)*(-B(N-1)*U(N-1,3)+ &
-                 B(N-2)*U(N-2,3))/DETER- &
-                 DETLT(N-3)*APS(N-1)*BPS(N-3)*B(N-3)*U(N-3,3)/DETER
-            FPS(N,1)=-DETLT(N-2)*BPS(N-2)*B(N-2)*U(N-2,1)/DETER
-            FPS(N,2)=-DETLT(N-2)*BPS(N-2)*B(N-2)*U(N-2,2)/DETER
-            FPS(N,3)=-DETLT(N-2)*BPS(N-2)*B(N-2)*U(N-2,3)/DETER
-	 
-            DO 35 I=1,N
-               DRDT(I+IB,1,RK)=DRDT(I+IB,1,RK)+FPS(I,1)/XI
-               DRDT(I+IB,2,RK)=DRDT(I+IB,2,RK)+FPS(I,2)/XI
-               DRDT(I+IB,3,RK)=DRDT(I+IB,3,RK)+FPS(I,3)/XI
- 35         CONTINUE
-         endif
+!         if (BROWN.EQ.1) then
+!            DETLT(1)=1.0
+!            DETLT(2)=APS(1)
+!            DETGT(N-1)=1.0
+!            DETGT(N-2)=APS(N-1)
+!            DO 20 I=3,(N-1)
+!               DETLT(I)=APS(I-1)*DETLT(I-1)- &
+!                    BPS(I-2)*BPS(I-2)*DETLT(I-2)
+!               DETGT(N-I)=APS(N-I+1)*DETGT(N-I+1)- &
+!                    BPS(N-I+1)*BPS(N-I+1)*DETGT(N-I+2)
+! 20         CONTINUE
+!            DETER=APS(N-1)*DETLT(N-1)-BPS(N-2)*BPS(N-2)*DETLT(N-2)
+!            FPS(1,1)=DETGT(2)*BPS(1)*B(2)*U(2,1)/DETER
+!            FPS(1,2)=DETGT(2)*BPS(1)*B(2)*U(2,2)/DETER
+!            FPS(1,3)=DETGT(2)*BPS(1)*B(2)*U(2,3)/DETER
+!            FPS(2,1)=DETGT(2)*BPS(1)*(B(1)*U(1,1)- &
+!                 B(2)*U(2,1))/DETER+ &
+!                 DETGT(3)*APS(1)*BPS(2)*B(3)*U(3,1)/DETER
+!            FPS(2,2)=DETGT(2)*BPS(1)*(B(1)*U(1,2)- &
+!                 B(2)*U(2,2))/DETER+ &
+!                 DETGT(3)*APS(1)*BPS(2)*B(3)*U(3,2)/DETER
+!            FPS(2,3)=DETGT(2)*BPS(1)*(B(1)*U(1,3)- &
+!                 B(2)*U(2,3))/DETER+ &
+!                 DETGT(3)*APS(1)*BPS(2)*B(3)*U(3,3)/DETER
+!            DO 30 I=3,(N-2)
+!               FPS(I,1)=(DETLT(I-1)*DETGT(I)*BPS(I-1)*(-B(I)*U(I,1)+B(I-1)*U(I-1,1))+ &
+!                    DETLT(I-1)*DETGT(I+1)*APS(I-1)*BPS(I)*B(I+1)*U(I+1,1)- &
+!                    DETLT(I-2)*DETGT(I)*APS(I)*BPS(I-2)*B(I-2)*U(I-2,1)- &
+!                    DETLT(I-2)*DETGT(I+1)*( &
+!                    (BPS(I-2)**2.)*BPS(I)*B(I+1)*U(I+1,1)- &
+!                    (BPS(I)**2.)*BPS(I-2)*B(I-2)*U(I-2,1)))/DETER
+!               FPS(I,2)=(DETLT(I-1)*DETGT(I)*BPS(I-1)*(-B(I)*U(I,2)+B(I-1)*U(I-1,2))+ &
+!                    DETLT(I-1)*DETGT(I+1)*APS(I-1)*BPS(I)*B(I+1)*U(I+1,2)- &
+!                    DETLT(I-2)*DETGT(I)*APS(I)*BPS(I-2)*B(I-2)*U(I-2,2)- &
+!                    DETLT(I-2)*DETGT(I+1)*( &
+!                    (BPS(I-2)**2.)*BPS(I)*B(I+1)*U(I+1,2)- &
+!                    (BPS(I)**2.)*BPS(I-2)*B(I-2)*U(I-2,2)))/DETER
+!               FPS(I,3)=(DETLT(I-1)*DETGT(I)*BPS(I-1)*(-B(I)*U(I,3)+B(I-1)*U(I-1,3))+ &
+!                    DETLT(I-1)*DETGT(I+1)*APS(I-1)*BPS(I)*B(I+1)*U(I+1,3)- &
+!                    DETLT(I-2)*DETGT(I)*APS(I)*BPS(I-2)*B(I-2)*U(I-2,3)- &
+!                    DETLT(I-2)*DETGT(I+1)*( &
+!                    (BPS(I-2)**2.)*BPS(I)*B(I+1)*U(I+1,3)- &
+!                    (BPS(I)**2.)*BPS(I-2)*B(I-2)*U(I-2,3)))/DETER
+! 30         CONTINUE
+!            FPS(N-1,1)=DETLT(N-2)*BPS(N-2)*(-B(N-1)*U(N-1,1)+ &
+!                 B(N-2)*U(N-2,1))/DETER- &
+!                 DETLT(N-3)*APS(N-1)*BPS(N-3)*B(N-3)*U(N-3,1)/DETER
+!            FPS(N-1,2)=DETLT(N-2)*BPS(N-2)*(-B(N-1)*U(N-1,2)+ &
+!                 B(N-2)*U(N-2,2))/DETER- &
+!                 DETLT(N-3)*APS(N-1)*BPS(N-3)*B(N-3)*U(N-3,2)/DETER
+!            FPS(N-1,3)=DETLT(N-2)*BPS(N-2)*(-B(N-1)*U(N-1,3)+ &
+!                 B(N-2)*U(N-2,3))/DETER- &
+!                 DETLT(N-3)*APS(N-1)*BPS(N-3)*B(N-3)*U(N-3,3)/DETER
+!            FPS(N,1)=-DETLT(N-2)*BPS(N-2)*B(N-2)*U(N-2,1)/DETER
+!            FPS(N,2)=-DETLT(N-2)*BPS(N-2)*B(N-2)*U(N-2,2)/DETER
+!            FPS(N,3)=-DETLT(N-2)*BPS(N-2)*B(N-2)*U(N-2,3)/DETER
+!	 
+!            DO 35 I=1,N
+!               DRDT(I+IB,1,RK)=DRDT(I+IB,1,RK)+FPS(I,1)/XI
+!               DRDT(I+IB,2,RK)=DRDT(I+IB,2,RK)+FPS(I,2)/XI
+!               DRDT(I+IB,3,RK)=DRDT(I+IB,3,RK)+FPS(I,3)/XI
+! 35         CONTINUE
+!         endif
 	  
 
-! Setup the B component
-     
-         DO 40 I=1,(N-1)
-            BLAM(I)=B(I)*XI*(U(I,1)*(DRDT(I+1+IB,1,RK)-DRDT(I+IB,1,RK))+ &
-                 U(I,2)*(DRDT(I+1+IB,2,RK)-DRDT(I+IB,2,RK))+ &
-                 U(I,3)*(DRDT(I+1+IB,3,RK)-DRDT(I+IB,3,RK)))+ &
-                 XI*C*(B(I)**.2-L0**2.)
- 40      CONTINUE
-		    PRINT*, BLAM
-		
+! Setup the B component     
+        DO 40 I=1,(N-1)
+           BLAM(I)=B(I)*XI*(U(I,1)*(DRDT(I+1+IB,1,RK)-DRDT(I+IB,1,RK))+ &
+                U(I,2)*(DRDT(I+1+IB,2,RK)-DRDT(I+IB,2,RK))+ &
+                U(I,3)*(DRDT(I+1+IB,3,RK)-DRDT(I+IB,3,RK)))+ &
+                XI*C*(B(I)**.2-L0**2.)
+40      CONTINUE
+
 ! Calculate the new rates of change
-         call dgtsv((N-1),1,ASUB,ADIAG,ASUPER,BLAM,(N-1),INFO)
+        call dgtsv((N-1),1,ASUB,ADIAG,ASUPER,BLAM,(N-1),INFO)		 
          DRDT(1+IB,1,RK)=DRDT(1+IB,1,RK)+BLAM(1)*B(1)*U(1,1)/XI
          DRDT(1+IB,2,RK)=DRDT(1+IB,2,RK)+BLAM(1)*B(1)*U(1,2)/XI
          DRDT(1+IB,3,RK)=DRDT(1+IB,3,RK)+BLAM(1)*B(1)*U(1,3)/XI
