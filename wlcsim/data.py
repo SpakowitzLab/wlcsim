a5b8679915eeb55b2cf152a25e51f5778f463a86
Bruno Beltran <brunobeltran0@gmail.com>
2016-10-31 14:19:03 -0700

    METHOD: wlcsim.analyze.get_com_msd

--- wlcsim/data.py
+++ wlcsim/data.py
@@ -181,9 +181,8 @@ class Sim:
 
     @cached_property
     def center_of_mass(self):
-        num_time_points, ndim, N = self.r.shape
         # "equal mass" beads, so just average positions
-        return np.sum(self.r, axis=2)/N
+        return np.mean(self.r, axis=2)
 
     @cached_property
     def rtime_idxs(self):
