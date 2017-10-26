0004f8656ab942293975aa10e48958ad3290932d
Bruno Beltran <brunobeltran0@gmail.com>
2017-10-26 14:50:38 -0700

    WIP: replaces appears to work, missing call read*(wlc_p*) handling

--- wlcsim/fortran_parser.py
+++ wlcsim/fortran_parser.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 
 import pandas as pd
+import numpy as np
 
 self_dir, self_file = os.path.split(os.path.realpath(__file__))
 src_dir = os.path.join(self_dir, '..', 'src')
@@ -130,10 +131,8 @@ def wlc_p_to_defines():
     compiler's constant folding step.
 
     This function extracts the wlcsim_params type, gets its default values from
-    params.set_param_defaults, deletes unused variables from this type and from
-    the defaults function, creates a 'defines.h' file, removes all mentions of
-    wlc_p from parameter lists, and replaces all uses of 'wlc_p%NAME' with
-    'wlc_p__NAME' by brute force regex.
+    params.set_param_defaults, making a DataFrame with all the information
+    needed for make_defines_inc to do it's job.
 
     Special care must be taken with variables like minWindow. These are used as
     arrays, one index for each move. We keep the defaults lines for these
@@ -173,7 +172,7 @@ def wlc_p_to_defines():
 
     defines = []
     defines_columns = ['define_name', 'name', 'indexer', 'value',
-                       'declaration comment', 'defaults comment',
+                       'declaration_comment', 'defaults_comment',
                        'type', 'type_size', 'shape', 'is_looped_on',
                        'keep_regardless']
     # not_found = []
@@ -187,8 +186,11 @@ def wlc_p_to_defines():
                 raise ValueError('Could not find default for variable ' + variable.name)
                 # not_found.append(variable.name)
             else:
-                value = defaults.value[var_ix]
-            dcom = defaults.comment[var_ix]
+                value = defaults.value[var_ix].values[0]
+            if value is None:
+                dcom = None
+            else:
+                dcom = defaults.comment[var_ix].values[0]
             defines.append((define_name, variable.name, None,
                     value, variable.comment, dcom, variable.type,
                     variable.type_size, variable.shape,
@@ -220,8 +222,8 @@ def wlc_p_to_defines():
                 raise ValueError('Could not find default for variable ' + variable.name)
                 # not_found.append(variable.name)
             else:
-                value = defaults.value[var_ix]
-            dcom = defaults.comment[var_ix]
+                value = defaults.value[var_ix].values[0]
+            dcom = defaults.comment[var_ix].values[0]
             defines.append((define_name, variable.name, indexer,
                     value, variable.comment, dcom, variable.type,
                     variable.type_size, variable.shape,
@@ -229,24 +231,243 @@ def wlc_p_to_defines():
                     variable.name in vars_to_keep))
     defines = pd.DataFrame(defines)
     defines.columns = defines_columns
+    return defines
 
-    active_defines = defines.loc[~(defines.is_looped_on | defines.keep_regardless)]
-    delete_declaration_lines(active_defines.name, os.path.join(src_dir, 'wlcsim', 'params.f03'))
-    delete_definition_lines(active_defines.name, os.path.join(src_dir, 'wlcsim', 'params.f03'))
+def make_defines_inc(active_defines):
+    """Takes defines from wlc_p_to_defines,
 
-    defines_file = os.path.join(src_dir, 'defines.h')
+    deletes unused variables from the wlcsim_params type and from the defaults
+    function, creates a 'defines.inc' file, and replaces all uses of 'wlc_p%NAME'
+    with 'wlc_p__NAME' by brute force regex.
+    """
+    defines_file = os.path.join(src_dir, 'defines.inc')
     if os.path.exists(defines_file):
         raise OSError(defines_file + ' already exists!')
     with open(defines_file, 'w') as f:
         f.write("\n\n#define PI 4*atan(1.0_dp)\n\n")
         for new_define in active_defines.itertuples():
-            f.write("VARIABLE COMMENT: {}\n")
-            f.write("DEFAULTS COMMENT: {}\n")
-            f.write("#define {} {}")
-    #TODO awk command here
+            f.write("! VARIABLE COMMENT: {}\n".format(new_define.declaration_comment))
+            f.write("! DEFAULTS COMMENT: {}\n".format(new_define.defaults_comment))
+            if new_define.type_size:
+                f.write("! TYPE: {}({})\n".format(new_define.type, new_define.type_size))
+            else:
+                f.write("! TYPE: {}\n".format(new_define.type))
+            f.write("#define {} {}\n".format(new_define.define_name, new_define.value))
+
+def replace_wlc_p_instances(active_defines):
+    # def shape_to_int(shape):
+    #     if shape == 'NDIM':
+    #         return len(xyz_names)
+    #     elif shape == 'NMOVETYPES':
+    #         return len(mc_moves)
+    #     else:
+    #         return int(shape)
+    affected_files = subprocess.check_output(['grep', '-iRl', 'wlc_p%', src_dir]).splitlines()
+    for file in affected_files:
+        # if os.path.basename(file) == b'defines.inc':
+        #     continue
+        if file[-2:] == b'.o':
+            continue
+        # add the include line at the top for defines.inc
+        with open(file, 'r') as original: data = original.read()
+        with open(file, 'w') as modified: modified.write('#include "../defines.inc"\n' + data)
+        names_in_replace_order = get_replace_order(active_defines.name.values)
+        # now replace all instances of wlc_p variables with their new values
+        for name in names_in_replace_order:
+            defines_with_name = active_defines[active_defines.name == name]
+            for define in defines_with_name.itertuples():
+                if define.shape is None:
+                    subprocess.check_output(['sed', '-i',
+                            's/wlc_p%' + define.name + '/' + define.define_name + '/gi',
+                            file])
+                else:
+                    subprocess.check_output(['sed', '-i',
+                            's/wlc_p%' + define.name + '(' + define.indexer + ')/'
+                            + define.define_name + '/gi', file])
+
+def get_replace_order(strings):
+    """Return a list of tuples corresponding to the defines that we are going to
+    replace, but in order such that all superstrings are replaced first."""
+    lengths = np.array([-len(string) for string in strings])
+    ix = np.argsort(lengths)
+    return np.array(strings)[ix]
+
+    # tree = SuperStringTree(strings)
+    # # pre order traversal gives superstrings first
+    # replace_order = []
+    # for string in reversed(tree.base.pre_order_values()):
+    #     if not string or string in replace_order:
+    #         continue
+    #     replace_order.append(string)
+    # return replace_order
+
+# class Node:
+#     def __init__(self, value):
+#         self.children = list()
+#         self.value = value
+#     def __str__(self, level=0):
+#             ret = "\t"*level + repr(self) + "\n"
+#             for child in self.children:
+#                 ret += child.__str__(level + 1)
+#             return ret
+#     def __repr__(self):
+#         return "<Tree Node, Value={}, len(Children)={}>".format(self.value, len(self.children))
+#     def pre_order_values(self):
+#         values = []
+#         values.append(self.value)
+#         for child in self.children:
+#             values += child.pre_order_values()
+#         return values
+#     def post_order_values(self):
+#         values = []
+#         for child in self.children:
+#             values += child.pre_order_values()
+#         values.append(self.value)
+#         return values
+#     @property
+#     def child_values(self):
+#         return [child.value for child in self.children]
+
+# class SuperStringNode(Node):
+#     # # this algorithm doesn't work, because for example say we insert
+#     # # "a", then "ab", then "b". "ab" will be a child of "a", so it won't
+#     # # get tested to see if it's a superstring of b
+#     def insert_string(self, string):
+#         was_inserted = False
+#         for i,child in enumerate(self.children):
+#             # if string is a superstring, so should be child of child
+#             if child.value in string:
+#                 child.insert_string(string)
+#                 was_inserted = True
+#             # if string is a substring, so should be parent of child
+#             if string in child.value:
+#                 new_node = SuperStringNode(string)
+#                 new_node.children.append(child)
+#                 if self.value is not None:
+#                     self.children[i] = new_node
+#                     was_inserted = True
+#         # it was neither super nor substring of any children, so it's gets its 
+#         # own branch. the root gets every string as a direct child, but not here
+#         if not was_inserted and self.value is not None:
+#             self.children.append(SuperStringNode(string))
+#         return was_inserted
+
+# class SuperStringTree:
+#     def __init__(self, strings):
+#         self.base = SuperStringNode(None)
+#         self.all_strings = []
+#         for string in strings:
+#             self.base.insert_string(string)
+#             self.base.children.append(SuperStringNode(string))
+#             for old_string in self.all_strings:
+#                 self.base.children[-1].insert_string(old_string)
+#             self.all_strings.append(string)
+
+# def get_superstring_tree(strings):
+#     """Pre order traversal gives reversed "replace order"... """
+#     tree = Node(None)
+#     for s in strings:
+#         if len(tree.children) == 0:
+#             tree.children.append(Node(s))
+#             continue
+#         is_substring = True
+#         node = tree
+#         prev_node = tree
+#         while is_substring:
+#             if len(node.children) == 0:
+#                 node.children.append(Node(s))
+#             for i,child_node in enumerate(node.children):
+#                 if s in child_node.value:
+#                     new_node = Node(s)
+#                     new_node.children.append(child_node)
+#                     node.children[i] = new_node
+#                     is_substring = False
+#                     break
+#                 if child_node.value in s:
+#                     prev_node = node
+#                     node = child_node
+#                     break
+#             else:
+#                 is_substring = False
+#                 prev_node.children.append(Node(s))
+#     return tree
+
+# def get_substring_tree(strings):
+#     """Not very useful kind of tree."""
+#     tree = Node(None)
+#     for s in strings:
+#         if len(tree.children) == 0:
+#             tree.children.append(Node(s))
+#             continue
+#         is_substring = True
+#         node = tree
+#         prev_node = tree
+#         while is_substring:
+#             if len(node.children) == 0:
+#                 node.children.append(Node(s))
+#             for i,child_node in enumerate(node.children):
+#                 if child_node.value in s:
+#                     new_node = Node(s)
+#                     new_node.children.append(child_node)
+#                     node.children[i] = new_node
+#                     is_substring = False
+#                     break
+#                 if s in child_node.value:
+#                     prev_node = node
+#                     node = child_node
+#                     break
+#             else:
+#                 is_substring = False
+#                 prev_node.children.append(Node(s))
+#     return tree
+
+def perform_wlc_p_to_define_transform():
+    defines = wlc_p_to_defines()
+    active_defines = defines.loc[~(defines.is_looped_on | defines.keep_regardless)]
+    delete_declaration_lines(active_defines.name.values, os.path.join(src_dir, 'wlcsim', 'params.f03'),
+                             in_type='wlcsim_params')
+    delete_definition_lines(active_defines.name.values, os.path.join(src_dir, 'wlcsim', 'params.f03'),
+                            in_subroutine='set_param_defaults')
+    make_defines_inc(active_defines)
+    replace_wlc_p_instances(active_defines)
 
 
 
+def delete_declaration_lines(names, file, in_type=None):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    with open(file, 'w') as f:
+        pos = "before type"
+        for line in lines:
+            if in_type and pos == "before type":
+                if re.search("type\s+" + in_type, line):
+                    pos = "inside type"
+            elif in_type and pos == "inside type":
+                if re.search("end\s+type", line):
+                    pos = "after type"
+            if not in_type or pos == "inside type":
+                match = declaration(line)
+                if match and match[2].upper() in names:
+                    continue
+            f.write(line)
+
+def delete_definition_lines(names, file, in_subroutine=None):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    with open(file, 'w') as f:
+        pos = "before subroutine"
+        for line in lines:
+            if in_subroutine and pos == "before subroutine":
+                if re.search("subroutine\s+" + in_subroutine, line):
+                    pos = "inside subroutine"
+            elif in_subroutine and pos == "inside subroutine":
+                if re.search("end\s+subroutine", line):
+                    pos = "after subroutine"
+            if not in_subroutine or pos == "inside subroutine":
+                match = wlc_p_default(line)
+                if match and match[0].upper() in names:
+                    continue
+            f.write(line)
 
 def defines_to_wlc_p():
     """The inverse of wlc_p_to_defines. //TODO"""
@@ -257,44 +478,3 @@ def defines_to_wlc_p():
 
 # end do big code modifications
 #####}}}
-
-# # first get all parameter defaults
-# param_defaults_body = get_function_body('set_param_defaults', 'src/wlcsim/params.f03')
-# param_defaults = pd.Series([parse_definition(line) for line in
-#                             param_defaults_body if is_definition(line)])
-# param_vals = {p[0]: [] for p in param_defaults_definitions}
-# for p in param_defaults_definitions:
-#     param_vals[p[0]].append((p[1], p[2]))
-# # param_vals = {(p[0], p[1]): p[2] for p in param_defaults_definitions}
-
-# # now get all parameter definitions
-# param_lines = get_type_body('wlcsim_params', 'src/wlcsim/params.f03')
-# all_params = [variable_from_fortran_line(line) for line in param_lines]
-# all_params = [p for p in all_params if p is not None]
-
-# # now use the parameter definitions to unroll into the right number of
-# # variables for the array variables
-# unrolled_params = {}
-# unrolled_defaults = {}
-# for name, shape, comment in all_params:
-#     if shape is None:
-#         unrolled_params[name] = comment
-#         unrolled_defaults[name] = param_vals[name][1] # [1] gets just value
-#     elif shape == '3':
-#         unrolled_defaults[name] = param_vals[name][1] # [1] gets just value
-#         for dimension in num_to_dim.values():
-#             unrolled_params[name + '_' + dimension.upper()] = comment
-#     elif shape == 'NMOVETYPES':
-#         for move in mc_moves.values():
-#             unrolled_params[name + '_' + move.upper()] = comment
-#     else:
-#         raise ValueError("Unknown shape '" + shape + "' for variable '" + name + "'.")
-# param_name_to_comment = {p[0]: p[1] for p in all_params}
-
-# pval_d = {p[0]: param_vals[p[0]] for p in all_params}
-
-
-# print(all_params)
-
-
-
