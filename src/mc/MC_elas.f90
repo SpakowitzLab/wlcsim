a26323b2ad9c18ecf89bde9079e945c26be82cfa
qmacAstanford <qmac@stanford.edu>
2018-06-21 20:14:50 -0700

    Consolidated energy calculations into module

--- src/mc/MC_elas.f90
+++ src/mc/MC_elas.f90
@@ -11,6 +11,7 @@ subroutine MC_eelas(wlc_p,DEELAS,R,U,RP,UP,IB1,IB2,&
                     MCTYPE,WR,WRP)
 
 use params, only: dp, pi,wlcsim_params
+use MC_wlc, only: E_wlc, E_SSWLC, E_GAUSS
 implicit none
 type(wlcsim_params), intent(in) :: wlc_p
 real(dp), intent(in) :: R(3,WLC_P__NT)  ! Bead positions
@@ -22,8 +23,6 @@ integer, intent(in) :: IT1               ! Index of test bead 1
 integer, intent(in) :: IB2               ! Test bead position 2
 integer, intent(in) :: IT2               ! Index of test bead 2
 
-real(dp) :: U0(3,WLC_P__NT)  ! dummy variable for calculating u in wlc case
-real(dp) :: UP0(3,WLC_P__NT)  ! dummy variable for calculating u in wlc case
 real(dp) :: UNORM,U1U2
 
 real(dp), intent(out) :: DEELAS(4)   ! Change in ECOM
@@ -79,89 +78,18 @@ real(dp) GI(3)
              print*, "You will need to update this section before use."
              print*, "Finish implementing IT1 and IT2"
              stop 1
-
-            U0(1,IT1M1) = R(1,IT1)-R(1,IT1M1)
-            U0(2,IT1M1) = R(2,IT1)-R(2,IT1M1)
-            U0(3,IT1M1) = R(3,IT1)-R(3,IT1M1)
-            UNORM = sqrt(U0(1,IT1M1)**2. + U0(2,IT1M1)**2. + U0(3,IT1M1)**2.)
-            U0(1,IT1M1) = U0(1,IT1M1)/UNORM
-            U0(2,IT1M1) = U0(2,IT1M1)/UNORM
-            U0(3,IT1M1) = U0(3,IT1M1)/UNORM
-
-            U0(1,IT1) = R(1,IT1 + 1)-R(1,IT1)
-            U0(2,IT1) = R(2,IT1 + 1)-R(2,IT1)
-            U0(3,IT1) = R(3,IT1 + 1)-R(3,IT1)
-            UNORM = sqrt(U0(1,IT1)**2. + U0(2,IT1)**2. + U0(3,IT1)**2.)
-            U0(1,IT1) = U0(1,IT1)/UNORM
-            U0(2,IT1) = U0(2,IT1)/UNORM
-            U0(3,IT1) = U0(3,IT1)/UNORM
-
-            UP0(1,IT1) = RP(1,IT1 + 1)-RP(1,IT1)
-            UP0(2,IT1) = RP(2,IT1 + 1)-RP(2,IT1)
-            UP0(3,IT1) = RP(3,IT1 + 1)-RP(3,IT1)
-            UNORM = sqrt(UP0(1,IT1)**2. + UP0(2,IT1)**2. + UP0(3,IT1)**2.)
-            UP0(1,IT1) = UP0(1,IT1)/UNORM
-            UP0(2,IT1) = UP0(2,IT1)/UNORM
-            UP0(3,IT1) = UP0(3,IT1)/UNORM
-
-            U1U2 = U0(1,IT1M1)*U0(1,IT1) + U0(2,IT1M1)*U0(2,IT1) + U0(3,IT1M1)*U0(3,IT1)
-            ! only bending energy in WLC, others are init to zero, so sum in total energy calculation later will be no-op
-            DEELAS(1) = DEELAS(1) + EB*U1U2
-            U1U2 = U(1,IT1M1)*UP0(1,IT1) + U(2,IT1M1)*UP(2,IT1) + U(3,IT1M1)*UP(3,IT1)
-            DEELAS(1) = DEELAS(1)-EB*U1U2
+             DEELAS(1) = E_wlc(RP(:,IT1M1), RP(:,IT1), RP(:,IT1P1), EB ) 
+             DEELAS(1) = DEELAS(1) - E_wlc(R(:,IT1M1), R(:,IT1), R(:,IT1P1), EB) 
 
          elseif (wlc_p%SIMTYPE == 2) then
 
-            DR(1) = R(1,IT1)-R(1,IT1M1)
-            DR(2) = R(2,IT1)-R(2,IT1M1)
-            DR(3) = R(3,IT1)-R(3,IT1M1)
-            DRPAR = DR(1)*U(1,IT1M1) + DR(2)*U(2,IT1M1) + DR(3)*U(3,IT1M1)
-
-            DRPERP(1) = DR(1)-DRPAR*U(1,IT1M1)
-            DRPERP(2) = DR(2)-DRPAR*U(2,IT1M1)
-            DRPERP(3) = DR(3)-DRPAR*U(3,IT1M1)
-            !U1U2 = U(1,IT1M1)*U(1,IT1) + U(2,IT1M1)*U(2,IT1) + U(3,IT1M1)*U(3,IT1)
-
-            GI(1) = (U(1,IT1)-U(1,IT1M1)-ETA*DRPERP(1))
-            GI(2) = (U(2,IT1)-U(2,IT1M1)-ETA*DRPERP(2))
-            GI(3) = (U(3,IT1)-U(3,IT1M1)-ETA*DRPERP(3))
-
-            DEELAS(1) = DEELAS(1)-0.5_dp*EB*(GI(1)**2 + GI(2)**2 + GI(3)**2)
-            DEELAS(2) = DEELAS(2)-0.5_dp*EPAR*(DRPAR-GAM)**2
-            DEELAS(3) = DEELAS(3)-0.5_dp*EPERP*(DRPERP(1)**2 + DRPERP(2)**2. + DRPERP(3)**2)
-
-            DR(1) = RP(1,IT1)-R(1,IT1M1)
-            DR(2) = RP(2,IT1)-R(2,IT1M1)
-            DR(3) = RP(3,IT1)-R(3,IT1M1)
-            DRPAR = DR(1)*U(1,IT1M1) + DR(2)*U(2,IT1M1) + DR(3)*U(3,IT1M1)
-
-            DRPERP(1) = DR(1)-DRPAR*U(1,IT1M1)
-            DRPERP(2) = DR(2)-DRPAR*U(2,IT1M1)
-            DRPERP(3) = DR(3)-DRPAR*U(3,IT1M1)
-            !U1U2 = U(1,IT1M1)*UP(1,IT1) + U(2,IT1M1)*UP(2,IT1) + U(3,IT1M1)*UP(3,IT1)
-
-            GI(1) = (UP(1,IT1)-U(1,IT1M1)-ETA*DRPERP(1))
-            GI(2) = (UP(2,IT1)-U(2,IT1M1)-ETA*DRPERP(2))
-            GI(3) = (UP(3,IT1)-U(3,IT1M1)-ETA*DRPERP(3))
-
-            ! in ssWLC, we have, bending, parallel extension, and perpendicular extension energy, which we must sum later to get
-            ! total energy
-            DEELAS(1) = DEELAS(1) + 0.5_dp*EB*(GI(1)**2 + GI(2)**2 + GI(3)**2)
-            DEELAS(2) = DEELAS(2) + 0.5_dp*EPAR*(DRPAR-GAM)**2
-            DEELAS(3) = DEELaS(3) + 0.5_dp*EPERP*(DRPERP(1)**2 + DRPERP(2)**2 + DRPERP(3)**2)
+             !function E_SSWLC(R,RM1,U,UM1,EB,EPAR,EPERP,ETA,GAM)
+             DEELAS = DEELAS + E_SSWLC(RP(:,IT1),RP(:,IT1M1),UP(:,IT1),UP(:,IT1M1),EB,EPAR,EPERP,ETA,GAM)
+             DEELAS = DEELAS + E_SSWLC( R(:,IT1), R(:,IT1M1), U(:,IT1), U(:,IT1M1),EB,EPAR,EPERP,ETA,GAM)
 
          elseif (wlc_p%SIMTYPE == 3) then
-
-            DR(1) = R(1,IT1)-R(1,IT1M1)
-            DR(2) = R(2,IT1)-R(2,IT1M1)
-            DR(3) = R(3,IT1)-R(3,IT1M1)
-            ! in gaussian chain, there's only parallel stretching energy. DEELAS init'd to zeros, so sum(DEELAS) == DEELAS(2) later
-            DEELAS(2) = DEELAS(2)-0.5*EPAR*(DR(1)**2. + DR(2)**2. + DR(3)**2.)
-            DR(1) = RP(1,IT1)-R(1,IT1M1)
-            DR(2) = RP(2,IT1)-R(2,IT1M1)
-            DR(3) = RP(3,IT1)-R(3,IT1M1)
-            DEELAS(2) = DEELAS(2) + 0.5*EPAR*(DR(1)**2. + DR(2)**2. + DR(3)**2.)
-
+             DEELAS(2) = DEELAS(2) + E_GAUSS(RP(:,IT1),RP(:,IT1M1),EPAR)
+             DEELAS(2) = DEELAS(2) - E_GAUSS( R(:,IT1), R(:,IT1M1),EPAR)
          endif
       endif
 
@@ -176,92 +104,25 @@ real(dp) GI(3)
          ! determined by the r's. Thus we're not worried about double counting the energy change here since the energy change
          ! should be zero by definition if IB1 = =IB2.
          if (wlc_p%SIMTYPE == 1.AND.((IB2 /= 1).OR.(WLC_P__RING))) then
-            if (IB2 == 1) then
-                IT2M1 = WLC_P__NB
-            else
-                IT2M1 = IT2 - 1
-            endif
-            Print*, "This section is out of date"
-            print*, "The variable IT2M1 is never used!"
-            stop
-            U0(1,IT2-1) = R(1,IT2)-R(1,IT2-1)
-            U0(2,IT2-1) = R(2,IT2)-R(2,IT2-1)
-            U0(3,IT2-1) = R(3,IT2)-R(3,IT2-1)
-            UNORM = sqrt(U(1,IT2-1)**2. + U(2,IT2-1)**2. + U(3,IT2-1)**2.)
-            U0(1,IT2-1) = U0(1,IT2-1)/UNORM
-            U0(2,IT2-1) = U0(2,IT2-1)/UNORM
-            U0(3,IT2-1) = U0(3,IT2-1)/UNORM
-
-            U0(1,IT2) = R(1,IT2 + 1)-R(1,IT2)
-            U0(2,IT2) = R(2,IT2 + 1)-R(2,IT2)
-            U0(3,IT2) = R(3,IT2 + 1)-R(3,IT2)
-            UNORM = sqrt(U0(1,IT2)**2. + U0(2,IT2)**2. + U0(3,IT2)**2.)
-            U0(1,IT2) = U0(1,IT2)/UNORM
-            U0(2,IT2) = U0(2,IT2)/UNORM
-            U0(3,IT2) = U0(3,IT2)/UNORM
-
-            UP0(1,IT2-1) = RP(1,IT2)-RP(1,IT2-1)
-            UP0(2,IT2-1) = RP(2,IT2)-RP(2,IT2-1)
-            UP0(3,IT2-1) = RP(3,IT2)-RP(3,IT2-1)
-            UNORM = sqrt(UP0(1,IT2-1)**2. + UP0(2,IT2-1)**2. + UP0(3,IT2-1)**2.)
-            UP0(1,IT2-1) = UP0(1,IT2-1)/UNORM
-            UP0(2,IT2-1) = UP0(2,IT2-1)/UNORM
-            UP0(3,IT2-1) = UP0(3,IT2-1)/UNORM
-
-            U1U2 = U0(1,IT2-1)*U0(1,IT2) + U0(2,IT2-1)*U0(2,IT2) + U0(3,IT2-1)*U0(3,IT2)
-            DEELAS(1) = DEELAS(1) + EB*U1U2
-            U1U2 = UP0(1,IT2-1)*U0(1,IT2) + UP0(2,IT2-1)*U0(2,IT2) + UP0(3,IT2-1)*U0(3,IT2)
-            DEELAS(1) = DEELAS(1)-EB*U1U2
+             if (IB2 == 1) then
+                 IT2M1 = WLC_P__NB
+             else
+                 IT2M1 = IT2 - 1
+             endif
+             Print*, "This section is out of date"
+             print*, "The variable IT2M1 is never used!"
+             stop
+             DEELAS(1) = E_wlc(RP(:,IT2M1),RP(:,IT2),RP(:,IT2P1),EB)  
+             DEELAS(1) = DEELAS(1) - E_wlc(R(:,IT2M1),R(:,IT2),R(:,IT2P1),EB) 
 
          elseif (wlc_p%SIMTYPE == 2) then
-
-            DR(1) = R(1,IT2P1)-R(1,IT2)
-            DR(2) = R(2,IT2P1)-R(2,IT2)
-            DR(3) = R(3,IT2P1)-R(3,IT2)
-            DRPAR = DR(1)*U(1,IT2) + DR(2)*U(2,IT2) + DR(3)*U(3,IT2)
-
-            DRPERP(1) = DR(1)-DRPAR*U(1,IT2)
-            DRPERP(2) = DR(2)-DRPAR*U(2,IT2)
-            DRPERP(3) = DR(3)-DRPAR*U(3,IT2)
-            !U1U2 = U(1,IT2)*U(1,IT2 + 1) + U(2,IT2)*U(2,IT2 + 1) + U(3,IT2)*U(3,IT2 + 1)
-
-            GI(1) = (U(1,IT2P1)-U(1,IT2)-ETA*DRPERP(1))
-            GI(2) = (U(2,IT2P1)-U(2,IT2)-ETA*DRPERP(2))
-            GI(3) = (U(3,IT2P1)-U(3,IT2)-ETA*DRPERP(3))
-
-            DEELAS(1) = DEELAS(1)-0.5_dp*EB*(GI(1)**2. + GI(2)**2. + GI(3)**2.)
-            DEELAS(2) = DEELAS(2)-0.5_dp*EPAR*(DRPAR-GAM)**2.
-            DEELAS(3) = DEELAS(3)-0.5_dp*EPERP*(DRPERP(1)**2. + DRPERP(2)**2. + DRPERP(3)**2.)
-
-            DR(1) = R(1,IT2P1)-RP(1,IT2)
-            DR(2) = R(2,IT2P1)-RP(2,IT2)
-            DR(3) = R(3,IT2P1)-RP(3,IT2)
-            DRPAR = DR(1)*UP(1,IT2) + DR(2)*UP(2,IT2) + DR(3)*UP(3,IT2)
-
-            DRPERP(1) = DR(1)-DRPAR*UP(1,IT2)
-            DRPERP(2) = DR(2)-DRPAR*UP(2,IT2)
-            DRPERP(3) = DR(3)-DRPAR*UP(3,IT2)
-            !U1U2 = UP(1,IT2)*U(1,IT2 + 1) + UP(2,IT2)*U(2,IT2 + 1) + UP(3,IT2)*U(3,IT2 + 1)
-
-            GI(1) = (U(1,IT2P1)-UP(1,IT2)-ETA*DRPERP(1))
-            GI(2) = (U(2,IT2P1)-UP(2,IT2)-ETA*DRPERP(2))
-            GI(3) = (U(3,IT2P1)-UP(3,IT2)-ETA*DRPERP(3))
-
-            DEELAS(1) = DEELAS(1) + 0.5_dp*EB*(GI(1)**2. + GI(2)**2 + GI(3)**2)
-            DEELAS(2) = DEELAS(2) + 0.5_dp*EPAR*(DRPAR-GAM)**2.
-            DEELAS(3) = DEELAS(3) + 0.5_dp*EPERP*(DRPERP(1)**2. + DRPERP(2)**2. + DRPERP(3)**2.)
+             !function E_SSWLC(R,RM1,U,UM1,EB,EPAR,EPERP,ETA,GAM)
+             DEELAS = DEELAS + E_SSWLC(RP(:,IT2P1),RP(:,IT2),UP(:,IT2P1),UP(:,IT2),EB,EPAR,EPERP,ETA,GAM)
+             DEELAS = DEELAS - E_SSWLC(R(:,IT2P1),R(:,IT2),U(:,IT2P1),U(:,IT2),EB,EPAR,EPERP,ETA,GAM)
 
          elseif (wlc_p%SIMTYPE == 3) then
-
-            DR(1) = R(1,IT2P1)-R(1,IT2)
-            DR(2) = R(2,IT2P1)-R(2,IT2)
-            DR(3) = R(3,IT2P1)-R(3,IT2)
-            DEELAS(2) = DEELAS(2)-0.5*EPAR*(DR(1)**2. + DR(2)**2. + DR(3)**2.)
-            DR(1) = R(1,IT2P1)-RP(1,IT2)
-            DR(2) = R(2,IT2P1)-RP(2,IT2)
-            DR(3) = R(3,IT2P1)-RP(3,IT2)
-            DEELAS(2) = DEELAS(2) + 0.5*EPAR*(DR(1)**2. + DR(2)**2. + DR(3)**2.)
-
+             DEELAS(2) = DEELAS(2) + E_GAUSS(RP(:,IT2P1),RP(:,IT2),EPAR)
+             DEELAS(2) = DEELAS(2) - E_GAUSS( R(:,IT2P1), R(:,IT2),EPAR)
          endif
 
       endif
---
+++ src/mc/MC_wlc.f90
@@ -0,0 +1,66 @@
+#include "../defines.inc"
+!---------------------------------------------------------------*
+!
+!      Chain backbone potential between two beads
+!
+! E_wlc(RM1,R,RP1,EB)
+! E_SSWLC(R,RM1,U,UM1,EB,EPAR,EPERP,ETA,GAM)
+! E_GAUSS(R,RM1,EPAR)
+!---------------------------------------------------------------
+module MC_wlc
+implicit none
+contains
+
+function E_wlc(RM1,R,RP1,EB)
+    use params, only: dp
+    implicit none
+    real(dp), intent(in), dimension(3) :: RM1 ! R of bead i-1
+    real(dp), intent(in), dimension(3) :: R ! R of bead i
+    real(dp), intent(in), dimension(3) :: RP1 ! R of bead i+1
+    real(dp), intent(in) :: EB
+    real(dp) E_wlc
+    real(dp), dimension(3) :: U0, U
+
+    U0 = R - RM1
+    U0 = U0/norm2(U0)
+    U = RP1 - R
+    U = U/norm2(U)
+    E_wlc = EB*DOT_PRODUCT(U,U0)
+end function E_wlc
+
+function E_SSWLC(R,RM1,U,UM1,EB,EPAR,EPERP,ETA,GAM)
+    use params, only: dp
+    implicit none
+    real(dp), intent(in), dimension(3) :: RM1 ! R of bead i-1
+    real(dp), intent(in), dimension(3) :: R ! R of bead i
+    real(dp), intent(in), dimension(3) :: UM1 ! U of bead i-1
+    real(dp), intent(in), dimension(3) :: U ! U of bead i
+    real(dp), intent(in) :: EB, EPAR, EPERP, ETA, GAM ! SSWLC constants
+    real(dp), dimension(3) :: E_SSWLC
+    real(dp) GI(3)
+    real(dp) DR(3)
+    real(dp) DRPAR
+    real(dp) DRPERP(3)
+
+    DR = R-RM1
+    DRPAR = dot_product(DR,UM1)
+    DRPERP = DR - DRPAR*UM1
+    GI = U - UM1 - ETA*DRPERP
+    E_SSWLC(1)=0.5_dp*EB*dot_product(GI,GI)
+    E_SSWLC(2)=0.5_dp*EPAR*(DRPAR-GAM)**2
+    E_SSWLC(3)=0.5_dp*EPERP*dot_product(DRPERP,DRPERP)
+end function E_SSWLC
+
+function E_GAUSS(R,RM1,EPAR)
+    use params, only: dp
+    implicit none
+    real(dp), intent(in), dimension(3) :: RM1 ! R of bead i-1
+    real(dp), intent(in), dimension(3) :: R ! R of bead i
+    real(dp), intent(in) :: EPAR
+    real(dp) E_GAUSS 
+    real(dp) DR(3)
+    E_GAUSS = 0.5_dp*EPAR*dot_product(DR,DR)
+    ! in gaussian chain, there's only parallel stretching energy. DEELAS init'd to zeros, so sum(DEELAS) == DEELAS(2) later
+end function E_GAUSS
+
+end module
