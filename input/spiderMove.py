b89ecfd79a25f5f007f41cdf90fbd8918ce119b2
qmacAstanford <qmac@stanford.edu>
2018-06-23 13:46:37 -0700

    New Feature: python for precal loops and spiders
    makeloops.py generates the bindpairs vector
    sipderMove uses the bindpairs vector to generate spiders

---
+++ input/spiderMove.py
@@ -0,0 +1,266 @@
+import numpy as np
+
+def blank_slide_section(center_bead):
+    #print('blank section ',center_bead,' created')
+    return {'L_slide_bead':center_bead,
+            'R_slide_bead':center_bead,
+            'L_knee':None,
+            'L_toe':None,
+            'R_knee':None,
+            'R_toe':None}
+
+def combine(subsumed_section_id,subsuming_section_id,sections,membership):
+    #print("subsumed_section_id",subsumed_section_id,"subsuming_section_id",subsuming_section_id)
+    subsumed = sections[subsumed_section_id]
+    subsuming = sections[subsuming_section_id]
+    
+    # make entire memebership region subsumed_section_id
+    left = min(subsumed['L_slide_bead'],subsuming['L_slide_bead'])
+    right = max(subsumed['R_slide_bead'],subsuming['R_slide_bead'])
+    membership[left:right+1] = subsuming_section_id
+    subsuming['L_slide_bead'] = left
+    subsuming['R_slide_bead'] = right
+    
+    del sections[subsumed_section_id]
+    #print("combining done",membership,"    combinedSection:")
+    #print_section(subsuming)
+
+def checkForCollision(bead,membership,section_id,slide_section,sections):
+    polymerLength=len(bindpairs)
+    if bead<0: # encounter end of polymer
+        membership[0:slide_section['L_slide_bead'] ]=section_id
+        slide_section['L_slide_bead']=0
+        return 'PolymerEnd'
+    if bead>=polymerLength: # encounter end of polymer
+        membership[slide_section['R_slide_bead']:]=section_id
+        slide_section['R_slide_bead']=polymerLength-1
+        return 'PolymerEnd'
+    if membership[bead]>=0: # combine with another section
+        combine(section_id,membership[bead],sections,membership)
+        return 'Combined'
+    return False
+
+def print_section(section):
+    print(str(section['L_toe'])+'-'+str(section['L_knee'])+'-'
+          +str(section['L_slide_bead'])+'='+str(str(section['R_slide_bead']))
+          +'-'+str(section['R_knee'])+'-'+str(section['R_toe']))
+
+
+def is_independent_polymer(end1,end2,bindpairs):
+    low=min(end1,end2)
+    high=max(end1,end2)
+    if np.min(bindpairs[low:high+1])<low:
+        return False
+    if np.max(bindpairs[low:high+1])>high:
+        return False
+    return True
+
+
+def growLeg(section_id,bindpairs,membership,sections,leglength=10,moveable_loop=50,direction=1):
+    slide_section=sections[section_id]
+    if moveable_loop<4*leglength:
+        print('moveable_loop',moveable_loop)
+        print('leglength',leglength)
+        raise ValueError('moveable_loop must be >= 4*leglength')
+    if direction not in [-1,1]:
+        raise ValueError('direction must be in [-1,1]')
+    
+    LorR = {1:'R_slide_bead',-1:'L_slide_bead'}[direction]
+    
+    #print('Growing Leg of section',section_id,' from',slide_section[LorR],{1:'up',-1:'down'}[direction])
+    knee=slide_section[LorR]
+    for ii in range(0,leglength):
+        knee=knee+direction # grow leg by one
+        collision = checkForCollision(knee,membership,section_id,slide_section,sections)
+        if collision:
+            return collision
+        otherend = bindpairs[knee]
+        if otherend == -1: # normal chain
+            continue
+        if (abs(otherend-knee) <= moveable_loop 
+            # loop is short enough
+            and is_independent_polymer(knee,otherend,bindpairs)
+            # loop isn't interanlly connected elsewhere
+            and (otherend-knee)*direction >=0): # loop is in same direction as leg
+            knee = otherend
+            collision = checkForCollision(knee,membership,section_id,slide_section,sections)
+            if collision:
+                return collision
+            continue
+        
+        # didn't have normal step or subsumed loop
+        # extend slide region
+        # create new section with two more legs
+        membership[min(slide_section[LorR],knee):max(slide_section[LorR],knee)+1] = section_id
+        membership[otherend]=otherend
+        slide_section[LorR]=knee 
+        sections[otherend]=blank_slide_section(otherend) # name the section by its starting bead
+
+        whatHappend = growLeg(otherend,bindpairs,membership,sections,
+                              leglength=leglength,moveable_loop=moveable_loop,direction=1)     
+        if whatHappend != 'Combined':
+            growLeg(otherend,bindpairs,membership,sections,
+                    leglength=leglength,moveable_loop=moveable_loop,direction=-1)
+        
+        # the leg we've built so far must be slid instead, continue building from there
+        #slide_section[LorR]=knee 
+        #print('Retry Growing Leg of section',section_id,' from',slide_section[LorR],{1:'up',-1:'down'}[direction])
+        return growLeg(section_id,bindpairs,membership,sections,leglength=leglength,moveable_loop=moveable_loop,direction=direction)
+        
+    
+    toe=knee
+    for ii in range(0,leglength):
+        toe=toe+direction # grow leg by one
+        collision = checkForCollision(toe,membership,section_id,slide_section,sections)
+        if collision:
+            return collision
+        #print('toe',toe,'membership',membership[toe],'other end',bindpairs[toe]) 
+        otherend = bindpairs[toe]
+        if otherend == -1: # normal chain
+            continue
+            
+        # loop subsumed into leg
+        if (abs(otherend-toe) <= moveable_loop  
+            # loop is short enough
+            and is_independent_polymer(toe,otherend,bindpairs) 
+            # loop isn't interanlly connected elsewhere
+            and (otherend-toe)*direction >=0): # loop is in same direction as leg
+            toe = otherend
+            collision = checkForCollision(toe,membership,section_id,slide_section,sections)
+            if collision:
+                return collision
+            continue
+        
+        # didn't have normal step or subsumed loop
+        # create new section with two more legs
+        membership[min(slide_section[LorR],toe):max(slide_section[LorR],toe)+1] = section_id
+        membership[otherend]=otherend
+        slide_section[LorR]=toe
+        sections[otherend]=blank_slide_section(otherend) # name the section by its starting bead
+        whatHappend = growLeg(otherend,bindpairs,membership,sections,
+                              leglength=leglength,moveable_loop=moveable_loop,direction=1)
+        if whatHappend != 'Combined':
+            growLeg(otherend,bindpairs,membership,sections,
+                    leglength=leglength,moveable_loop=moveable_loop,direction=-1)
+        
+        # the leg we've built so far must be slid instead, continue building from there
+        #print('Retry Growing Leg of section',section_id,' from',slide_section[LorR],{1:'up',-1:'down'}[direction])
+        return growLeg(section_id,bindpairs,membership,sections,leglength=leglength,moveable_loop=moveable_loop,direction=direction)
+    
+    LorRknee = {1:'R_knee',-1:'L_knee'}[direction]
+    LorRtoe = {1:'R_toe',-1:'L_toe'}[direction]
+    slide_section[LorRknee]=knee
+    slide_section[LorRtoe]=toe
+    return 'normal'
+    
+
+def makeSections(bindpairs,end1,leglength=5,moveable_loop=25):
+    end2=bindpairs[end1]
+    if end2==-1:
+        raise ValueError('end1 must be bound to something')
+    sections={}
+    membership=np.zeros(len(bindpairs)).astype(int)-1
+
+    
+    sections[end1] = blank_slide_section(end1)
+    membership[end1]=end1
+    sections[end2] = blank_slide_section(end2)
+    membership[end2]=end2
+    keys = list(sections.keys())
+
+    for section_id in keys:
+        whatHappend = growLeg(section_id,bindpairs,membership,sections,
+                              leglength=leglength,moveable_loop=moveable_loop,direction=1)        
+        if whatHappend != 'Combined':
+            growLeg(section_id,bindpairs,membership,sections,
+                    leglength=leglength,moveable_loop=moveable_loop,direction=-1)
+
+    ID_list = np.sort(list(sections.keys()))
+    for ii in range(len(ID_list)-1):
+        rightID= ID_list[ii+1]
+        leftID=ID_list[ii]
+        seperation = sections[rightID]['L_slide_bead'] - sections[leftID]['R_slide_bead']
+        overlapped_legs = sections[rightID]['L_toe'] <= sections[leftID]['R_toe']
+        if seperation < moveable_loop or overlapped_legs:
+            combine(leftID,rightID,sections,membership)
+    return sections
+
+
+def print_sections(sections):
+    for ID in sections:
+        print_section(sections[ID])
+
+
+import sys
+from contextlib import contextmanager
+@contextmanager
+def file_or_stdout(file_name):
+    if file_name is None:
+        yield sys.stdout
+    else:
+        with open(file_name, 'w') as out_file:
+            yield out_file
+            
+def print_spiders_for_fortran(spiders,filename=None,offset=0):
+    #file = open(filename,"w")
+    with file_or_stdout(filename) as file:
+        print(len(spiders),file=file)
+        for spider in spiders:
+            print(len(spider),file=file)
+            legs=[]
+            moved_regions=[]
+            for key in spider:
+                section=spider[key]
+                moved_retion = [section['L_slide_bead'],section['R_slide_bead']]
+                print(section['L_slide_bead']+offset,' ',
+                      section['R_slide_bead']+offset,file=file)
+                if section['L_toe'] != None:
+                    legs.append([section['L_slide_bead'],
+                                 section['L_knee'],
+                                 section['L_toe']])
+                    moved_retion[0]=section['L_toe']
+                if section['R_toe'] != None:
+                    legs.append([section['R_slide_bead'],
+                                 section['R_knee'],
+                                 section['R_toe']])
+                    moved_retion[1]=section['R_toe']
+                moved_regions.append(moved_retion)
+            print(len(legs),file=file)
+            for leg in legs:
+                for val in leg:
+                    print(val+offset,end=' ',file=file)
+                print('',file=file)
+            for moved_retion in moved_regions:
+                print(moved_retion[0]+offset,' ',
+                      moved_retion[1]+offset,file=file)
+
+
+def makeSpiders(bindpairs,leglength=5,moveable_loop=25):
+    min_slide_loop=moveable_loop
+    spiders = []
+    for ii in range(0,len(bindpairs)):
+        if bindpairs[ii] == -1:
+            continue
+
+        if bindpairs[ii] < ii+min_slide_loop:
+            continue
+
+        sections = makeSections(bindpairs,ii,leglength=leglength,moveable_loop=moveable_loop)
+        spiders.append(sections)
+        #print_sections(sections)
+        #print('-------')
+    return spiders
+    
+
+
+if len(sys.argv) ==2:
+    fname= sys.argv[1]
+    outName=None
+elif len(sys.argv) ==3:
+    fname= sys.argv[1]
+    outName=sys.argv[2]
+    
+bindpairs = np.loadtxt(fname).astype(int)
+bindpairs[bindpairs != -1] = bindpairs[bindpairs != -1] - 1
+spiders = makeSpiders(bindpairs,leglength=5,moveable_loop=25)
+print_spiders_for_fortran(spiders,filename=outName,offset=1)
