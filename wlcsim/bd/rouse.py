7fb4aee7663255ddfec338c700eb6151162f5d27
Bruno Beltran <brunobeltran0@gmail.com>
2019-08-28 05:53:27 -0700

    BUGFIX: rouse now has correct init, calls jit_rouse

--- wlcsim/bd/rouse.py
+++ wlcsim/bd/rouse.py
@@ -1,11 +1,11 @@
-from bruno_util.runge_kutta import rkfm
+from bruno_util.runge_kutta import *
 
 from numba import jit
 import numpy as np
 
 from pathlib import Path
 
-def rouse(N, b, L, t0, tf, D, x0=None, hmax=None, hmin=None):
+def rouse(N, L, b, D, t, x0=None):
     r"""Simulate a Rouse polymer made of N beads free in solution.
 
     The polymer beads can be described by the discrete Rouse equations
@@ -27,17 +27,13 @@ def rouse(N, b, L, t0, tf, D, x0=None, hmax=None, hmin=None):
     def rouse_f(x, t):
         dx = np.diff(x, axis=0)
         zero = np.array([[0,0,0]])
-        return -k_over_xi*(np.concatenate([zero, dx]) + np.concatenate([dx, zero])) \
-
+        return -k_over_xi*(np.concatenate([zero, dx]) + np.concatenate([dx, zero]))
     if x0 is None:
         L0 = L/(N-1) # length per bead
         Lb = L0/b # kuhn lengths per bead
-        x0 = np.cumsum(np.random.normal(size=(N,3)), axis=0)
-    if hmax is None:
-        hmax = (tf-t0)/N
-    if hmin is None:
-        hmin=(tf - t0)/1e8
-    t, X = rkfm(rouse_f, D, t0, tf, x0, tol=1e-6, hmax=hmax, hmin=hmin)
-    return t, X
+        x0 = b*np.sqrt(Lb)*np.cumsum(np.random.normal(size=(N,3)), axis=0)
+    X = rk4_thermal_lena(rouse_f, D, t, x0)
+    return X
 
-
+def fast_rouse(N, L, b, D, t):
+    return jit_rouse(N, L, b, D, t)
