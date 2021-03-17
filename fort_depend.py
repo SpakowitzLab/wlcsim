e4ddd22a86f2d8950cea44466c1c9e2d21a8ff9a
Bruno Beltran <brunobeltran0@gmail.com>
2021-03-17 13:32:50 -0700

    MAKE: 2to3 fort_depend

--- fort_depend.py
+++ fort_depend.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import os
 import re
 
@@ -11,9 +11,9 @@ def run(files=None,verbose=True,overwrite=None,output=None,macros={},build='',co
 
     if verbose:
         for i in depends.keys():
-            print "\033[032m"+i+"\033[039m depends on :\033[034m"
-            for j in depends[i]: print "\t"+j
-            print "\033[039m"
+            print("\033[032m"+i+"\033[039m depends on :\033[034m")
+            for j in depends[i]: print("\t"+j)
+            print("\033[039m")
 
     if output is None:
         output = "makefile.dep"
@@ -27,7 +27,7 @@ def write_depend(outfile="makefile.dep",dep=[],overwrite=False,build='',compile_
     #Test file doesn't exist
     if os.path.exists(outfile):
         if not(overwrite):
-            print "\033[031mWarning file exists.\033[039m"
+            print("\033[031mWarning file exists.\033[039m")
             opt=raw_input("Overwrite? Y... for yes.")
         else:
             opt="y"
@@ -137,7 +137,7 @@ def get_depends(fob=[],m2f=[]):
             try:
                 tmp.append(m2f[j.lower()])
             except:
-                print "\033[031mWarning:\033[039m module \033[032m"+j+"\033[039m used in "+i.file_name+" not defined in any files. Skipping..."
+                print("\033[031mWarning:\033[039m module \033[032m"+j+"\033[039m used in "+i.file_name+" not defined in any files. Skipping...")
 
         deps[i.file_name]=tmp
 
